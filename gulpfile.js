f518d46c9d111daf12d0f4fbd87a204b670fffbd
Girobusan <private>
2025-01-16 17:53:21 +0300

    Clean up source, better build routine

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
 const { src, dest, watch, series } = require("gulp");
-var rename = require("gulp-rename");
+const rename = require("gulp-rename");
+const postcss = require("gulp-postcss");
 const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
 
 async function buildCSS() {
   return src("dev/scss/amzng.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/"))
     .pipe(dest("css/"));
 }
 
 async function buildDist() {
-  return (
-    src("dev/scss/amzng.scss")
-      .pipe(sass({ style: "compressed" }).on("error", sass.logError))
-      // .pipe(rename("amzng.min.css"))
-      .pipe(dest("css/"))
-  );
+  return src("dev/scss/amzng.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([require("autoprefixer")]))
+    .pipe(dest("css/"))
+    .pipe(dest("dist/"))
+    .pipe(postcss([require("postcss-minify")]))
+    .pipe(rename("amzng.min.css"))
+    .pipe(dest("css/"))
+    .pipe(dest("dist/"));
 }
 
+async function watchFiles() {
+  return watch("dev/scss/*.scss", buildCSS);
+}
+
+exports.watch = watchFiles;
 exports.css = buildCSS;
 exports.dist = buildDist;
-exports.watch = watch("dev/scss/*.scss", buildCSS);
